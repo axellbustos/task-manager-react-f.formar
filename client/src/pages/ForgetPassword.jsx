0a735c0a7a1c73873e91174ad324e776f591f885
axell.bustos <axell.bustos@hotmail.com.ar>
2023-01-30 17:13:45 -0300

    forget-password done

---
+++ client/src/pages/ForgetPassword.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const ForgetPassword = () => {
+  return (
+    <div>
+      <h1>Recupera tu cotraseña</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">Correo electronico</label>
+          <input type="email" id="email" placeholder="Ingrese su email" />
+        </div>
+        <button type="submit">Recupera tu contraseña </button>
+      </form>
+      <nav>
+        <Link to={"/register"}>No tienes una cuenta? Registrate</Link>
+        <Link to={"/"}>Estas registrado? Inicia sesion</Link>
+      </nav>
+    </div>
+  );
+};
