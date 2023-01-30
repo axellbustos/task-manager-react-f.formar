d1f0fda7f1482ac1e35f115dc20a915119f3fd2a
axell.bustos <axell.bustos@hotmail.com.ar>
2023-01-30 17:10:19 -0300

    register done

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div>
+         <h1>Creá tu cuenta</h1>
+      <form action="">
+        <div>
+            <label htmlFor="name">Nombre</label>
+            <input type="text" id="name" placeholder="Ingrese su Nombre" autoComplete='off'/>
+        </div>
+        <div>
+            <label htmlFor="email">Correo electronico</label>
+            <input type="email" id="email" placeholder="Ingrese su email" />
+        </div>
+        <div>
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" id="password" placeholder="Ingrese su contraseña" />
+        </div>
+        <div>
+            <label htmlFor="password2">Confirme su contraseña</label>
+            <input type="password" id="password2" placeholder="Ingrese su contraseña" />
+        </div>
+        <button type="submit" >Crear cuenta</button>
+      </form>
+      <nav>
+        <Link to={'/'}>Estas registrado? Inicia sesion</Link>
+      </nav>
+    </div>
+  )
+}
