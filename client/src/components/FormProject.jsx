58367c7d89da8c321acd71d2e7bc972a7774bb57
axell.bustos <axell.bustos@hotmail.com.ar>
2023-02-11 23:51:06 -0300

    projects store done

--- client/src/components/FormProject.jsx
+++ client/src/components/FormProject.jsx
@@ -1,27 +1,53 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
+import { useProjects } from '../hooks/useProjects'
+import { Alert } from './Alert'
 
-export const FormProject = () => {
+export const FormProject = () => { 
+    const {alert, handleShowAlert,storeProject} = useProjects()
+
+    const {formValues,handleInputChange,reset} = useForm({
+        name:"",
+        description:"",
+        dateExpire:"",
+        client:""
+    })
+    const {name,description, dateExpire, client}= formValues
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+
+        if ([name,description,dateExpire,client].includes("")) {
+            handleShowAlert("todos los campos son obligatorios")
+            return null
+          }
+
+        storeProject({name, description, dateExpire, client})
+    }
+
   return (
-    <form 
-    /* onSubmit={} */
+    <form className='form flex  flex-col flex-wrap items-center text-white h-60 p-10 my-20 '
+    onSubmit={handleSubmit}
     >
+        <h1 className='w-full text-sky-600 text-center text-3xl'>Agregar Proyecto</h1>
         <div>
             <label htmlFor='name'>Nombre del proyecto</label>
-            <input type="text" id='name' placeholder='Nombre del proyecto.'/>
+            <input type="text" id='name' placeholder='Nombre del proyecto.' value={name} onChange={handleInputChange} name="name"/>
         </div>
         <div>
             <label htmlFor='description'>Descripcion del proyecto</label>
-            <input type="text" id='description' placeholder='Descripcion del proyecto.' style={{resize:"none"}}/>
+            <input type="text" id='description' placeholder='Descripcion del proyecto.' style={{resize:"none"}} value={description} onChange={handleInputChange} name="description"/>
         </div>
         <div>
-            <label htmlFor='data-expire'>Fecha de entrega</label>
-            <input type="date" id='data-expire' />
+            <label htmlFor='dateExpire'>Fecha de entrega</label>
+            <input type="date" id='data-expire' value={dateExpire} onChange={handleInputChange} name="dateExpire" />
         </div>
         <div>
             <label htmlFor='client'>Nombre cliente</label>
-            <input type="text" id='client' placeholder='Nombre de cliente.'/>
+            <input type="text" id='client' placeholder='Nombre de cliente.' value={client} onChange={handleInputChange} name="client"/>
         </div>
-        <button>Actualizar/Guardar</button>
+        {alert.msg && <Alert {...alert}/>}
+        <button type="submit" className='button-submit disabled:bg-slate-500'>Actualizar/Guardar</button>
     </form>
   )
 }
--- client/src/context/ProjectsProvider.jsx
+++ client/src/context/ProjectsProvider.jsx
@@ -1,10 +1,23 @@
 //busca y trae todos los projectos relacionados a el usuario logeado
 import React, {createContext, useState} from 'react'
 import { clientAxios } from '../config/clientAxios';
+import { useNavigate} from 'react-router-dom'
+import Swal from 'sweetalert2'
 
 const ProjectsContext = createContext()
-
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
  const ProjectsProvider = ({children}) => {
+    const navigate = useNavigate()
     const [projects, setProjects] = useState([]);
     const [project, setProject] = useState({});
     const [alert, setAlert] = useState({});
@@ -67,8 +80,36 @@ const ProjectsContext = createContext()
             setLoading(false)
         }
     }
+    const storeProject = async (project)=>{
+        try {
+            const token = sessionStorage.getItem('token')
+            if (!token) {
+                return null
+            }
+            const config  ={
+                headers: {
+                    "Content-Type":"application/json",
+                    Authorization : token
+                }
+            }
+
+            const {data}=await clientAxios.post(`/projects`, project, config)
+            console.log(data);
+            setProjects([...projects, data.project])
+
+            Toast.fire({
+                icon : 'success',
+                title : data.msg
+            });
+            navigate('/projects')
+
+        } catch (error) {
+            console.error(error);
+            handleShowAlert(error.response ? error.response.data.msg : "error ...")
+        }
+    }
   return (
-    <ProjectsContext.Provider value={{loading, alert, handleShowAlert, projects, getProjects, project, getProject}}>
+    <ProjectsContext.Provider value={{loading, alert, handleShowAlert, projects, getProjects, project, getProject, storeProject}}>
         {children}
     </ProjectsContext.Provider>
   )
