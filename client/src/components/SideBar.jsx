6ec97a8143ce2851f442bd1f2ba50fbb422fc353
axell.bustos <axell.bustos@hotmail.com.ar>
2023-02-14 09:04:15 -0300

    styles 2 progress

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -3,7 +3,26 @@ import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   return (
-    <aside className=" text-center  w-1/4 bg-sky-200 sideBar">
+    <aside className="flex flex-col text-center  w-1/4 bg-sky-200 sideBar">
+      <div className="flex justify-center m-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-user-circle"
+          width="150"
+          height="150"
+          viewBox="0 0 24 24"
+          stroke-width="0.5"
+          stroke="#000000"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <circle cx="12" cy="12" r="9" />
+          <circle cx="12" cy="10" r="3" />
+          <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+        </svg>
+      </div>
       <Link to="createProject" className="p-1 flex m-1 justify-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +42,47 @@ export const SideBar = () => {
         </svg>
         Agregar Proyecto
       </Link>
+      <hr></hr>
+      <div className="mt-100">
+        <div className="flex justify-center m-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-settings"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            stroke-width="0.5"
+            stroke="#000000"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+            <circle cx="12" cy="12" r="3" />
+          </svg>
+          <p>Configuracion</p>
+        </div>
+        <div className="flex justify-center m-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-logout"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            stroke-width="0.5"
+            stroke="#000000"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+          </svg>
+          <p>Cerrar Sesion</p>
+        </div>
+      </div>
     </aside>
   );
 };
